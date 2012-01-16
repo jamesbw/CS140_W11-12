97c8f2d602e8485d44132270d3038fd415be2e13
James Whitbeck <james.b.whitbeck@gmail.com>
2012-01-16 14:22:35 -0800

    fixed point fix

--- pintos/src/threads/fixed_point.h
+++ pintos/src/threads/fixed_point.h
@@ -7,6 +7,15 @@
 typedef int int17_14t;
 
 #define F 16384 /* 2**14 */
+int fixed_point_fp_to_int_nearest (int17_14t x);
+int fixed_point_fp_to_int_zero (int17_14t x);
+int17_14t fixed_point_int_to_fp (int n);
+int17_14t fixed_point_add_fp_fp (int17_14t x, int17_14t y);
+int17_14t fixed_point_add_fp_int (int17_14t x, int n);
+int17_14t fixed_point_multiply_fp_fp (int17_14t x, int17_14t y);
+int17_14t fixed_point_multiply_fp_int (int17_14t x, int n);
+int17_14t fixed_point_divide_fp_fp (int17_14t x, int17_14t y);
+int17_14t fixed_point_divide_fp_int (int17_14t x, int n);
 
 
 int
