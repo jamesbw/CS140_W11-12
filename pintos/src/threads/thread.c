ead3e7f34295c29fbbf6cb0bfa35539059aebda6
James Whitbeck <james.b.whitbeck@gmail.com>
2012-01-16 17:11:55 -0800

    added priority to next_thread

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -630,7 +630,7 @@ next_thread_to_run (void)
   if (list_empty (&ready_list))
     return idle_thread;
   else
-    return list_entry (list_pop_front (&ready_list), struct thread, elem);
+    return return list_entry (list_max (&ready_list), &thread_priority_comparator, NULL);
 }
 
 /* Completes a thread switch by activating the new thread's page
