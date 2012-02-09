70057ce9504def2c63380196a8885eeb8e2ce729
James Whitbeck <james.b.whitbeck@gmail.com>
2012-02-08 19:13:50 -0800

    added a few syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -7,6 +7,9 @@
 #include <console.h>
 #include "pagedir.h"
 
+#include "devices/shutdown.h"
+#include "filesys/filesys.h"
+
 static void syscall_handler (struct intr_frame *);
 static void *translate_uaddr_to_kaddr (const void *vaddr);
 static void check_buffer_uaddr (const void *buf, int size);
@@ -22,7 +25,7 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   uint32_t * k_esp = (uint32_t *) translate_uaddr_to_kaddr(f->esp);
-  uint32_t syscall_number = (uint32_t) *(k_esp);
+  uint32_t syscall_number = (uint32_t) *(k_esp); //TODO check all these addresses too
   uint32_t arg1 = (uint32_t) *(k_esp + 1);
   uint32_t arg2 = (uint32_t) *(k_esp + 2);
   uint32_t arg3 = (uint32_t) *(k_esp + 3);
@@ -30,16 +33,37 @@ syscall_handler (struct intr_frame *f UNUSED)
   switch (syscall_number)
   {
       case SYS_HALT:
+      {
+        shutdown_power_off ();
+        break;
+      }
       case SYS_EXIT:
-      case SYS_EXEC:
       {
         printf ("%s: exit(%d)\n", thread_current ()->name, arg1);
         f->eax = arg1;
         thread_exit ();
+        break;
+      }
+      case SYS_EXEC:
+      {
+        f->eax = process_execute ( (char *) arg1);
+        break;
       }
       case SYS_WAIT:
+      {
+        f->eax = process_wait ( (tid_t) arg1);
+        break;
+      }
       case SYS_CREATE:
+      {
+        f->eax = filesys_create ( (char *) arg1, arg2);
+        break;
+      }
       case SYS_REMOVE:
+      {
+        f->eax = filesys_remove ( (char *) arg1);
+        break;
+      }
       case SYS_OPEN:
       case SYS_FILESIZE:
       case SYS_READ:
@@ -53,6 +77,7 @@ syscall_handler (struct intr_frame *f UNUSED)
           putbuf (translate_uaddr_to_kaddr(buf), size);
         f->eax = size;
         break;
+         //TODO if arg1 is not 1, ie console
         
       }
       case SYS_SEEK:
@@ -70,6 +95,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 static void *
 translate_uaddr_to_kaddr (const void *vaddr)
 {
+  //TODO kill process instead of failing ASSERT
   ASSERT (is_user_vaddr (vaddr)); // Not user address
   uint32_t *kaddr = pagedir_get_page (thread_current ()->pagedir, vaddr);
   ASSERT (kaddr != NULL); // Not mapped
@@ -79,6 +105,7 @@ translate_uaddr_to_kaddr (const void *vaddr)
 static void
 check_buffer_uaddr (const void *buf, int size)
 {
+  //TODO kill process instead of failing ASSERT
   int i;
   for (i = 0; i < size; ++i)
     ASSERT (translate_uaddr_to_kaddr (buf + i) != NULL);
