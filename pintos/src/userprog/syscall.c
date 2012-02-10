cbfde5209b0f16c02cecb4d0d14f745c7cffa5a4
Benjamin Shapero <bshapero@stanford.edu>
2012-02-09 20:15:30 -0800

    Set up the syscall_ helper functions and wrote all prototypes in syscall.h.  From now on, all implementation of system calls should be done inside the helper functions.

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -21,7 +21,7 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   uint32_t * k_esp = (uint32_t *) translate_uaddr_to_kaddr(f->esp);
   uint32_t syscall_number = (uint32_t) *(k_esp); //TODO check all these addresses too
@@ -33,62 +33,141 @@ syscall_handler (struct intr_frame *f UNUSED)
   {
       case SYS_HALT:
       {
-        shutdown_power_off ();
+	syscall_halt();
         break;
       }
       case SYS_EXIT:
       {
-        printf ("%s: exit(%d)\n", thread_current ()->name, arg1);
-        f->eax = arg1;
-        thread_exit ();
+	syscall_exit(f, arg1);
 	break;
       }
       case SYS_EXEC:
       {
-        f->eax = process_execute ( (char *) arg1);
+	syscall_exec(f, arg1);
         break;
       }
       case SYS_WAIT:
       {
-        f->eax = process_wait ( (tid_t) arg1);
+	syscall_wait(f, arg1);
         break;
       }
       case SYS_CREATE:
       {
-        f->eax = filesys_create ( (char *) arg1, arg2);
+	syscall_create(f, arg1, arg2);
         break;
       }
       case SYS_REMOVE:
       {
-        f->eax = filesys_remove ( (char *) arg1);
+	syscall_remove(f, arg1);
         break;
       }
       case SYS_OPEN:
+      {
+	syscall_open(f, arg1);
+	break;
+      }
       case SYS_FILESIZE:
+      {
+	syscall_filesize(f, arg1);
+	break;
+      }
       case SYS_READ:
+      {
+	syscall_read(f, arg1, arg2, arg3);
+	break;
+      }
       case SYS_WRITE:
       {
-        void *buf = (void *) arg2;
-        int size = arg3;
-        check_buffer_uaddr (buf, size);
-
-        if (arg1 ==1) //fd 1
-          putbuf (translate_uaddr_to_kaddr(buf), size);
-        f->eax = size;
-        break;
-         //TODO if arg1 is not 1, ie console
-        
+	syscall_write(f, arg1, arg2, arg3);
+        break;        
       }
       case SYS_SEEK:
+      {
+	syscall_seek(f, arg1, arg2);
+	break;
+      }
       case SYS_TELL:
+      {
+	syscall_tell(f, arg1);
+	break;
+      }
       case SYS_CLOSE:
+      {
+	syscall_close(f, arg1);
+	break;
+      }
       default:
       {
         printf ("system call!\n");
         thread_exit ();
+	break;
       }  
       
   }
+}
+
+void syscall_halt(void) {
+  shutdown_power_off ();
+  NOT_REACHED();
+}
+
+void syscall_exit(struct intr_frame *f, uint32_t status) {
+  printf ("%s: exit(%d)\n", thread_current ()->name, status);
+  f->eax = status;
+  thread_exit ();
+}
+
+void syscall_exec(struct intr_frame *f, uint32_t file) {
+  f->eax = process_execute ((char *) file);
+}
+
+void syscall_wait(struct intr_frame *f, uint32_t tid) {
+  f->eax = process_wait ( (tid_t) tid);
+}
+
+void syscall_create(struct intr_frame *f, uint32_t file, uint32_t i_size)
+{
+  f->eax = filesys_create ( (char *) file, i_size);
+}
+
+void syscall_remove(struct intr_frame *f, uint32_t file) {
+  f->eax = filesys_remove ( (char *) file);
+}
+
+void syscall_open(struct intr_frame *f, uint32_t file) {
+  //TODO
+}
+
+void syscall_filesize(struct intr_frame *f, uint32_t fd) {
+  //TODO
+}
+
+void syscall_read(struct intr_frame *f, uint32_t fd, uint32_t buffer,
+		  uint32_t length) {
+  //TODO
+}
+
+void syscall_write(struct intr_frame *f, uint32_t fd, uint32_t buffer,
+		   uint32_t length) {
+  void *buf = (void *) buffer;
+  int size = length;
+  check_buffer_uaddr (buf, size);
+  if (fd == 1) //fd 1
+    putbuf (translate_uaddr_to_kaddr(buf), size);
+  f->eax = size;
+  //TODO if arg1 is not 1, ie console
+}
+
+void syscall_seek(struct intr_frame *f, uint32_t fd, uint32_t position) {
+  //TODO
+}
+
+void syscall_tell(struct intr_frame *f, uint32_t fd) {
+  //TODO
+}
+
+void syscall_close(struct intr_frame *f, uint32_t fd) {
+  //TODO
 }
 
 static void *
--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -3,4 +3,22 @@
 
 void syscall_init (void);
 
+// Prototypes for each system call called by the handler.
+
+void syscall_halt (void) NO_RETURN;
+void syscall_exit (struct intr_frame *f, uint32_t status) NO_RETURN;
+void syscall_exec (struct intr_frame *f, uint32_t file);
+void syscall_wait (struct intr_frame *f, uint32_t tid);
+void syscall_create (struct intr_frame *f, uint32_t file, uint32_t i_size);
+void syscall_remove (struct intr_frame *f, uint32_t file);
+void syscall_open (struct intr_frame *f, uint32_t file);
+void syscall_filesize (struct intr_frame *f, uint32_t fd);
+void syscall_read (struct intr_frame *f, uint32_t fd, uint32_t buffer,
+		   uint32_t length); 
+void syscall_write(struct intr_frame *f, uint32_t fd, uint32_t buffer,
+		   uint32_t length); 
+void syscall_seek (struct intr_frame *f, uint32_t fd, uint32_t position);
+void syscall_tell (struct intr_frame *f, uint32_t fd);
+void syscall_close (struct intr_frame *f, uint32_t fd);
+
 #endif /* userprog/syscall.h */
