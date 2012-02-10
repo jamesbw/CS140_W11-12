68c57e96065da0badcee49d8041bdba64c73226c
Benjamin Shapero <bshapero@stanford.edu>
2012-02-09 20:42:43 -0800

    Moved the prototypes into syscall.c because of linking/compiling issues.

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,7 +12,23 @@
 static void syscall_handler (struct intr_frame *);
 static void *translate_uaddr_to_kaddr (const void *vaddr);
 static void check_buffer_uaddr (const void *buf, int size);
+// Prototypes for each system call called by the handler.
 
+void syscall_halt (void);
+void syscall_exit (struct intr_frame *f, uint32_t status);
+void syscall_exec (struct intr_frame *f, uint32_t file);
+void syscall_wait (struct intr_frame *f, uint32_t tid);
+void syscall_create (struct intr_frame *f, uint32_t file, uint32_t i_size);
+void syscall_remove (struct intr_frame *f, uint32_t file);
+void syscall_open (struct intr_frame *f, uint32_t file);
+void syscall_filesize (struct intr_frame *f, uint32_t fd);
+void syscall_read (struct intr_frame *f, uint32_t fd, uint32_t buffer,
+		   uint32_t length); 
+void syscall_write(struct intr_frame *f, uint32_t fd, uint32_t buffer,
+		   uint32_t length); 
+void syscall_seek (struct intr_frame *f, uint32_t fd, uint32_t position);
+void syscall_tell (struct intr_frame *f, uint32_t fd);
+void syscall_close (struct intr_frame *f, uint32_t fd);
 
 void
 syscall_init (void) 
@@ -108,7 +124,6 @@ syscall_handler (struct intr_frame *f)
 
 void syscall_halt(void) {
   shutdown_power_off ();
-  NOT_REACHED();
 }
 
 void syscall_exit(struct intr_frame *f, uint32_t status) {
