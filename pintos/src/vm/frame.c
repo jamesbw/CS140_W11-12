ede94d75d57e800d910e9f2ee2a198da558105ca
James Whitbeck <james.b.whitbeck@gmail.com>
2012-02-26 18:31:55 -0800

    moved clear_page down in evict

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -94,7 +94,6 @@ frame_evict (void)
     frame_to_evict->pinned = true;
     struct page *page_to_evict = page_lookup(frame_to_evict->owner_thread->supp_page_table, frame_to_evict->upage);
     ASSERT (pagedir_get_page (page_to_evict->pd, page_to_evict->vaddr));
-    pagedir_clear_page (page_to_evict->pd, page_to_evict->vaddr);
     lock_release (&frame_table_lock);
 
 
@@ -133,6 +132,7 @@ frame_evict (void)
 
 
     void *result_kpage = frame_to_evict->paddr;
+    pagedir_clear_page (page_to_evict->pd, page_to_evict->vaddr);
     frame_free (result_kpage);
     return result_kpage;
 
