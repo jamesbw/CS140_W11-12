6923f65f1b842bda05ac1cb1fddf3e69f298c6c3
James Whitbeck <james.b.whitbeck@gmail.com>
2012-02-28 14:35:52 -0800

    oops, forgot parens in run clock

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -188,9 +188,11 @@ run_clock (void)
     if (e != NULL) 
     {
       page_to_evict = hash_entry(e, struct page, frame_elem);
-      if (!pagedir_is_accessed(page_to_evict->pd, page_to_evict->vaddr)) 
+      if (!pagedir_is_accessed(page_to_evict->pd, page_to_evict->vaddr))
+      { 
         if (page_to_evict->pinned == false)
           return page_to_evict;
+      }
       else 
         pagedir_set_accessed(page_to_evict->pd, page_to_evict->vaddr, false);
     }
