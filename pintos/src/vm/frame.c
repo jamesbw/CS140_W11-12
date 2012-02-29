8065c410035cb57d667d08b84a55e4c91dd2b6b4
James Whitbeck <james.b.whitbeck@gmail.com>
2012-02-28 22:16:13 -0800

    replace page_to_evict->paddr with kpage because former is now NULL

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -89,14 +89,14 @@ frame_evict (void)
                 page_to_evict->type = SWAP;
                 page_to_evict->swap_slot = swap_allocate_slot ();
                 lock_acquire (&filesys_lock);
-                swap_write_page ( page_to_evict->swap_slot, page_to_evict->paddr);
+                swap_write_page ( page_to_evict->swap_slot, kpage);
                 lock_release (&filesys_lock);
             }
             break;
         case SWAP:
             page_to_evict->swap_slot = swap_allocate_slot ();
             lock_acquire (&filesys_lock);
-            swap_write_page ( page_to_evict->swap_slot, page_to_evict->paddr);
+            swap_write_page ( page_to_evict->swap_slot, kpage);
             lock_release (&filesys_lock);
             break;
         case MMAPPED:
@@ -105,7 +105,7 @@ frame_evict (void)
                 //copy back to disk
                 lock_acquire (&filesys_lock);
                 file_seek (page_to_evict->file, page_to_evict->offset);
-                file_write (page_to_evict->file, page_to_evict->paddr, page_to_evict->valid_bytes);
+                file_write (page_to_evict->file, kpage, page_to_evict->valid_bytes);
                 lock_release (&filesys_lock);
             }
             pagedir_set_dirty (page_to_evict->pd, page_to_evict->vaddr, false); 
