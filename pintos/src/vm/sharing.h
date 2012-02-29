b0f8d61e19b6f28362b529a9d7d575d2f25f9973
James Whitbeck <james.b.whitbeck@gmail.com>
2012-02-28 17:17:33 -0800

    sharing

--- pintos/src/vm/sharing.h
+++ pintos/src/vm/sharing.h
@@ -1,6 +1,19 @@
 #ifndef VM_SHARING_H
 #define VM_SHARING_H
 
+#include <hash.h>
+#include <list.h>
 
+struct hash executable_table;
+
+struct shared_executable
+{
+	block;
+	offset;
+	struct hash_elem elem;
+};
+
+void sharing_clear_accessed_bit ();
+void sharing_get_accessed_bit ();
 
 #endif
