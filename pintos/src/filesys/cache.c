479f1ff9aa14754459c8352b48b97ee64663a15d
James Whitbeck <james.b.whitbeck@gmail.com>
2012-03-10 13:14:18 -0800

    updated makefile

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -1,1 +1,15 @@
 #include "cache.h"
+#include "threads/synch.h"
+#include "devices/block.h"
+
+struct cached_block block_cache[CACHE_SIZE];
+
+
+
+void 
+cache_init (void)
+{
+	int i;
+	for (i = 0; i < CACHE_SIZE; i ++)
+		lock_init (&block_cache[i]->lock);
+}
--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -1,5 +1,20 @@
 #ifndef FILESYS_CACHE_H
 #define FILESYS_CACHE_H
 
+#include "devices/block.h"
+#include "threads/synch.h"
+
+#define CACHE_SIZE 64 // 64 sectors
+
+struct cached_block
+{
+	uint8_t data[BLOCK_SECTOR_SIZE];
+	block_sector_t sector;
+	struct lock lock;
+};
+
+struct cached_block block_cache[CACHE_SIZE];
+
+void cache_init (void);
 
 #endif
