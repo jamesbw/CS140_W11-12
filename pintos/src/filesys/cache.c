c93c11430106942f041549d90c12407dc4c7d76e
James Whitbeck <james.b.whitbeck@gmail.com>
2012-03-12 20:59:47 -0700

    fixed locking in read-ahead

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -50,9 +50,9 @@ read_ahead_func (void *aux UNUSED)
 	struct list_elem *e;
 	struct cached_block *b;
 	struct queued_sector *queued_sector;
+	lock_acquire (&read_ahead_lock);
 	while (true)
 	{
-		lock_acquire (&read_ahead_lock);
 		if (!list_empty (&read_ahead_queue))
 		{
 			e = list_pop_front (&read_ahead_queue);
@@ -61,6 +61,7 @@ read_ahead_func (void *aux UNUSED)
 			b = cache_insert (queued_sector->sector);
 			lock_release (&b->lock);
 			free (queued_sector);
+			lock_acquire (&read_ahead_lock);
 		}
 		else
 		{
