fa727818c088740546cd85652780e9b09ff5445b
James Whitbeck <james.b.whitbeck@gmail.com>
2012-03-12 23:48:11 -0700

    more printfs

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -243,8 +243,10 @@ cache_insert (block_sector_t sector)
 		if (b->dirty)
 		{
 			block_write (fs_device, b->old_sector, b->data);
+			printf ("Writing out block %d\n", b->old_sector);
 		}
 		block_read (fs_device, b->sector, b->data);
+		printf ("Reading in block %d\n", b->sector);
 		b->IO_needed = false;
 		b->accessed = false;
 		b->dirty = false;
