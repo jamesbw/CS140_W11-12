498a17b1d82fc3655fd5febf72f1fb24a3eb507e
Benjamin Shapero <bshapero@stanford.edu>
2012-03-15 20:38:21 -0700

    Working on syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -44,8 +44,8 @@ void syscall_tell (struct intr_frame *f, uint32_t fd);
 void syscall_close (struct intr_frame *f, uint32_t fd);
 void syscall_mmap (struct intr_frame *f, uint32_t fd, uint32_t vaddr);
 void syscall_munmap (struct intr_frame *f, uint32_t mapid);
-void syscall_chdir (struct intr_frame *f, uint32_t fd, uint32_t dir_name);
-void syscall_mkdir (struct intr_frame *f, uint32_t fd, uint32_t dir_name);
+void syscall_chdir (struct intr_frame *f, uint32_t dir_name);
+void syscall_mkdir (struct intr_frame *f, uint32_t dir_name);
 void syscall_readdir (struct intr_frame *f, uint32_t fd, uint32_t name);
 void syscall_isdir (struct intr_frame *f, uint32_t fd);
 void syscall_inumber (struct intr_frame *f, uint32_t fd);
@@ -414,13 +414,13 @@ syscall_munmap (struct intr_frame *f UNUSED, uint32_t mapid)
 
 
 void 
-syscall_chdir (struct intr_frame *f UNUSED, uint32_t fd, uint32_t dir_name UNUSED)
+syscall_chdir (struct intr_frame *f UNUSED, uint32_t dir_name UNUSED)
 {
   
 }
 
 void 
-syscall_mkdir (struct intr_frame *f UNUSED, uint32_t fd, uint32_t dir_name UNUSED)
+syscall_mkdir (struct intr_frame *f UNUSED, uint32_t dir_name UNUSED)
 {
   char *name = (char *) dir_name;
   verify_uaddr (name);
