0bf0006298ead569434e3885bab7a699de2f539f
James Whitbeck <james.b.whitbeck@gmail.com>
2012-03-16 16:45:38 -0700

    writing removed inode back as zero

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -584,14 +584,14 @@ inode_close (struct inode *inode)
       /* Deallocate blocks if removed. */
       if (inode->removed) 
         {
-          free_map_release (inode->sector, 1);
           // free_map_release (inode->data.start,
           //                   bytes_to_sectors (inode->data.length));
           int num_sectors = bytes_to_sectors (inode->length);
           inode_release_allocated_sectors (inode, num_sectors); 
           uint8_t buf[BLOCK_SECTOR_SIZE];
-          memcpy (buf, inode, sizeof (*inode));
+          memset (buf, 0, BLOCK_SECTOR_SIZE);
           block_write (fs_device, inode->sector, buf);
+          free_map_release (inode->sector, 1);
         }
 
       free (inode); 
